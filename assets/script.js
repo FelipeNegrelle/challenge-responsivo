afe7f5d42964aaf7dfda969d6cb7149a2a82bc25
Felipe Negrelle <felipenegrelleramos@gmail.com>
2021-07-27 11:52:12 -0300

    commit de arquivos 27-07

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,11 @@ const botao__mobile = document.getElementById('botao__mobile');
 
 function toggleMenu() {
     const nav = document.querySelector('nav');
+    const header = document.querySelector('.cabecalho');
+    const headerTitulo = document.querySelector('.cabecalho__titulo');
     nav.classList.toggle('active');
+    header.classList.toggle('color');
+    headerTitulo.classList.toggle('color');
 }
 
 botao__mobile.addEventListener('click', toggleMenu);
